1ba3ab7a3ac60d5edaf7e779c4dfadc9c87c1d44
atacanjacques <atacan.jacques@gmail.com>
2020-03-12 10:21:47 +0100

    cors

--- src/app.js
+++ src/app.js
@@ -11,6 +11,9 @@ const bodyParser = require('body-parser');
 server.use(bodyParser.urlencoded());
 server.use(bodyParser.json());
 
+const cors = require('cors');
+server.use(cors());
+
 const postRoute = require('./api/routes/postRoute');
 postRoute(server);
 
